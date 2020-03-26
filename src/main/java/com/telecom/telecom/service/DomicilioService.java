30b3bf76ce3a2d9e1b53dd23c26e014a4c6c6c76
lorefior <56115031+lorefior@users.noreply.github.com>
2020-03-26 10:06:35 -0300

    service

--- src/main/java/com/telecom/telecom/service/DomicilioService.java
+++ src/main/java/com/telecom/telecom/service/DomicilioService.java
@@ -1,7 +1,11 @@
 package com.telecom.telecom.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.telecom.telecom.model.Domicilio;
 import com.telecom.telecom.repository.DomicilioRepository;
 
 @Service
@@ -9,4 +13,10 @@ public class DomicilioService {
 
 	@Autowired
 	DomicilioRepository domicilioRepository;
+	
+	public Optional buscarDomicilio(long id) {
+		Optional<Domicilio> domicilio;
+		domicilio = domicilioRepository.findById(id);
+		return domicilio;
+	}
 }
